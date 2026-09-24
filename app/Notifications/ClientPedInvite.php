fbd818aa6bf7b0c182fe6c02f6f9970b24b7b47b
Grazia <graziabaiamonte19@gmail.com>
2026-09-24 11:03:37 +0200

    Tipizza il notifiable di ClientPedInvite per soddisfare PHPStan

--- app/Notifications/ClientPedInvite.php
+++ app/Notifications/ClientPedInvite.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use App\Models\Client;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -25,7 +26,7 @@ class ClientPedInvite extends Notification implements ShouldQueue
         return ['mail'];
     }
 
-    public function toMail(object $notifiable): MailMessage
+    public function toMail(User $notifiable): MailMessage
     {
         $url = url(route('password.reset', [
             'token' => $this->token,
